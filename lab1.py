2011885e3b09f1c839da8c611169610e7ec170eb
Pradeep Dahal <pradeep.dahal222@gmail.com>
2019-05-12 11:00:40 +0545

    dbms-labpractice-2

---
+++ lab1.py
@@ -0,0 +1,20 @@
+#problem to solve to give the loan to the bank customer
+import sys
+
+loan=int(input("Enter customer's loan amount"))
+income=int(input("Enter the monthly income of the user"))
+papers=(input("Is there any house or property papers(yes/no)").lower())
+if(loan>500000):
+    input("Reject Loan")
+    sys.exit()
+    
+elif(income<15000):
+    input("Reject Loan")
+    sys.exit()
+    
+elif(papers!='yes'):
+    input("Reject Loan")
+    sys.exit()
+    
+else:
+    print("Accept Loan")
